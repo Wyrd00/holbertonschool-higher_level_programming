9db96fd0b41f6a850e0ef638c9c38ed0af6225c7
Amy <amy.tai0120@gmail.com>
2018-04-10 23:03:22 +0000

    fixing allll the pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(1, 90):
-	if i % 10 > i / 10 and i != 89:
-		print("{:02d}, ".format(i), end="")
+        if i % 10 > i / 10 and i != 89:
+                print("{:02d}, ".format(i), end="")
 print(i)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-	upper = ""
-	for i in str:
-		if ord(i) >= ord('a') and ord(i) <= ord('z'):
-			upper += chr(ord(i) - 32)
-		else:
-			upper += i
-	print("{}".format(upper), end="")
-	print("")
+        upper = ""
+        for i in str:
+                if ord(i) >= ord('a') and ord(i) <= ord('z'):
+                        upper += chr(ord(i) - 32)
+                else:
+                        upper += i
+        print("{}".format(upper), end="")
+        print("")
