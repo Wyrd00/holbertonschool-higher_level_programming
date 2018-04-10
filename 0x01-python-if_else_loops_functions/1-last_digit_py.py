dad97bf042972dc66be43cf4c3b03abab721090c
Amy <amy.tai0120@gmail.com>
2018-04-10 23:46:34 +0000

    remodify to fit with pep compliancy

--- 0x01-python-if_else_loops_functions/1-last_digit_py.py
+++ 0x01-python-if_else_loops_functions/1-last_digit_py.py
@@ -5,9 +5,10 @@ if number < 0:
         nummod = number % -10
 else:
         nummod = number % 10
+print("Last digit of {} is {} and is ".format(number, nummod), end="")
 if nummod > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, nummod))
-if nummod < 6 and nummod != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, nummod))
-if nummod == 0:
-        print("Last digit of {} is {} and is 0".format(number, 0))
+        print("greater than 5")
+elif nummod == 0:
+        print("0")
+else:
+        print("less than 6 and not 0")
