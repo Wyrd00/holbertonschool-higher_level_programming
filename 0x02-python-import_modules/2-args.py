a5999f76ff2ef235e0c00ebd9dba612b5f0bb2d9
Amy <amy.tai0120@gmail.com>
2018-04-12 23:29:23 +0000

    added __main__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-print("{} ".format(argc), end="")
-if argc == 0:
-        print("arguments.")
-elif argc == 1:
-        print("argument:")
-        print("{}: {}".format(1, sys.argv[1]))
-else:
-        print(":")
-        for i in range(1, len(sys.argv)):
-                print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+        import sys
+        argc = len(sys.argv) - 1
+        print("{} ".format(argc), end="")
+        if argc == 0:
+                print("arguments.")
+        elif argc == 1:
+                print("argument:")
+                print("{}: {}".format(1, sys.argv[1]))
+        else:
+                print("arguments:")
+                for i in range(1, len(sys.argv)):
+                        print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
-#sys.argv[1:] = [int(x) for x in sys.argv[1:]]
-#num = sys.argv
-#argc = len(sys.argv)
-num = 0
-total = 0
-for i in range(1, len(sys.argv)):
-        num = int(sys.argv[i])
-        total += num
-print(total)
+if __name__ == "__main__":
+        import sys
+        num = 0
+        total = 0
+        for i in range(1, len(sys.argv)):
+                num = int(sys.argv[i])
+                total += num
+        print(total)
