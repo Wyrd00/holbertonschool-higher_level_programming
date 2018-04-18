461186045d3b95f5d1e9b0d65317791dc1e9801e
Amy <amy.tai0120@gmail.com>
2018-04-18 01:52:48 +0000

    initial commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+        max_v = 0
+        if len(my_list) == 0:
+                return None
+        for i in my_list:
+                if max_v < i:
+                        max_v = i
+        return (max_v)
