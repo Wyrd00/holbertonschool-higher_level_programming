a39c911867ef6f51f4501c08c16b55b7e0ccf0ce
Amy <amy.tai0120@gmail.com>
2018-04-18 02:11:05 +0000

    changed initial value of max_v

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-        max_v = 0
         if len(my_list) == 0:
                 return None
+        max_v = my_list[0]
         for i in my_list:
                 if max_v < i:
                         max_v = i
