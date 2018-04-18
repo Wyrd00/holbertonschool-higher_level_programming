890945a440eda127d3ad0fda9197405a9e879e8f
Amy <amy.tai0120@gmail.com>
2018-04-18 01:34:28 +0000

    returned string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+        new_str = ''
         for i in my_string:
-                if i == 'C' or i == 'c':
-                        continue
-                else:
-                        print("{}".format(i), end="")
+                if i != 'C' and i != 'c':
+                        new_str += i
+        return new_str
