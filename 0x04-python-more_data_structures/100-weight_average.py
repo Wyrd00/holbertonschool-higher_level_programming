7f9388b3ab9cf25eeed271c5ccde33858e8e5cbc
Amy <amy.tai0120@gmail.com>
2018-04-20 05:29:48 +0000

    slightly less hideous

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
         if my_list:
-                multiplied, add_num, divisor = 1, 0, 0
+                total_weighted, total_tup = 0, 0
                 for tup in my_list:
-                        for elem in tup:
-                                multiplied *= elem
-                                if elem == tup[-1]:
-                                        divisor += elem
-                        add_num += multiplied
-                        multiplied = 1
-        return add_num / divisor
+                        (num, weighted) = tup
+                        total_weighted += weighted
+                        total_tup += (num * weighted)
+                print("{}: {}".format(total_weighted, total_tup))
+                return total_tup/total_weighted
+        return 0
