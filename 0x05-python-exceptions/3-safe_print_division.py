e79e2d08c2e5a6fdfa6d7e79d25c469b6c8fcfd3
Amy <amy.tai0120@gmail.com>
2018-04-30 22:45:31 +0000

    Q3-Q5

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def safe_print_division(a, b):
+        try:
+                print("Inside result: {0:.1f}".format(a / b))
+                result = a / b
+        except ZeroDivisionError:
+                print("Inside result: None")
+                result =  None
+        finally:
+                return result
