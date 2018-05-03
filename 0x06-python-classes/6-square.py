510c71ce5403819c56f2e9f7381f5b43e0e19b9b
Amy <amy.tai0120@gmail.com>
2018-05-03 17:58:27 +0000

    try me

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,23 +5,22 @@ class Square:
         """Creating a class called Square"""
         def __init__(self, size=0, position=(0, 0)):
                 """Creating a special method for Square"""
-                self.__size = size
-                self.__position = position
+                self.size = size
+                self.position = position
 
         def area(self):
                 """Creating a public instance method called area"""
                 return self.__size * self.__size
 
         def my_print(self):
-                """Creating a public instance method called my_print"""
-                if self.__size == 0:
-                        print()
-                for m in range(self.__position[1]):
-                        print()
+            """Creating a public instance method called my_print"""
+            if self.size == 0:
+                print()
+            for m in range(self.position[1]):
+                print()
 
-                for j in range(self.__size):
-                        print("{}{}".format(" " * self.__position[0], "#" * self.__size), end="")
-                        print()
+            for j in range(self.__size):
+                print("{}{}".format(" " * self.position[0], "#" * self.size))
 
         def position(self, size=0, position=(0, 0)):
                 """Creating a public instance method called position"""
@@ -29,15 +28,14 @@ class Square:
 
         @property
         def size(self):
-                """Property for attribute size"""
-                return self.__size
+            """Property for attribute size"""
+            return self.__size
 
         @size.setter
         def size(self, value):
                 """Setter for attribute size"""
                 if type(value) != int:
-                        print("size must be an integer", end="")
-                        raise TypeError
+                        raise TypeError("size must be an integer")
                 elif value < 0:
                         print("size must be >= 0", end="")
                         raise ValueError
@@ -51,10 +49,12 @@ class Square:
 
         @position.setter
         def position(self, value):
+                x, y = value
                 """Setter for attribute position"""
-                if type(value) is tuple and len(value) == 2 and type(value[0]) is int\
-                and type(value[1]) is int and value[0] > 0 and value[1] > 0:
+                if isinstance(value, tuple) and len(value) == 2 and \
+                isinstance(x, int) and isinstance(x, int) \
+                and x >= 0 and y >= 0:
                         self.__position = value
-                else: 
-                        print("position must be a tuple of 2 positive integers", end="")
-                        raise TypeError
+                else:
+                    print("position must be a tuple of 2 positive integers")
+                    raise TypeError
