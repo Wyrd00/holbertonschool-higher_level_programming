877effd5d9ea848495ceeab5c8350066287503eb
Amy <amy.tai0120@gmail.com>
2018-05-08 01:50:44 +0000

    fixed error

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,16 +4,19 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 2, 2, 59, 1]), 59)
 
     def test_max_negative(self):
         self.assertEqual(max_integer([-5, 1, 2, 3, 4]), 4)
-
-    def test_isdigit(self):
-        self.assetFalse(max_integer([1, 'tehe', 3]).isdigit())
+        self.assertEqual(max_integer([-5, -5, -5]), -5)
+        self.assertEqual(max_integer([-5, -4, -2, -4]), -2)
 
     def test_empty(self):
         self.assertIsNone(max_integer([]))
@@ -21,5 +24,5 @@ class TestMaxInteger(unittest.TestCase):
     def test_bool(self):
         self.assertEqual(max_integer([True, 42, False, 1, 2]), 42)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
