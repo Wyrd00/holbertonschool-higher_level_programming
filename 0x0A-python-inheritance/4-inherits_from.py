13255173d52ff9a545bcf641fef8eaec79a2bbe1
Amy <amy.tai0120@gmail.com>
2018-05-17 17:45:31 +0000

    change to issintance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ Module for checking if class is directly or indirectly inherited
 
 def inherits_from(obj, a_class):
     """returns true if obj is subclass of a_class, else false"""
-    return issubclass(obj, a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
