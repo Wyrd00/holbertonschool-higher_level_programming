f7a16a3f6bcb8bfe2ec248821e38f27fbeec8ba5
Wyrd00 <amy.tai0120@gmail.com>
2018-06-26 18:27:37 -0700

    12-15

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,4 @@ FROM tv_show_genres
 RIGHT OUTER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre.id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
