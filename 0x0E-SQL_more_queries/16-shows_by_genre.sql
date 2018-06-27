a645c2f6200144e9fe55ccd77cb1eb4434481342
Wyrd00 <amy.tai0120@gmail.com>
2018-06-26 17:59:51 -0700

    mistype

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
-ON tv_show.id=tv_show_genres.show_id
+ON tv_shows.id=tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres
 ON tv_genres.id=tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
