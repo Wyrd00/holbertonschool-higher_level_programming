aaadb7cf5a752c6323df1be796ba2b252145f108
Amy <amy.tai0120@gmail.com>
2018-07-20 04:16:59 +0000

    pep8 line length

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
         db=argv[3])
     cur = db.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY id ASC")
+        "SELECT cities.id, cities.name, states.name FROM cities JOIN states\
+         ON cities.state_id = states.id ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
