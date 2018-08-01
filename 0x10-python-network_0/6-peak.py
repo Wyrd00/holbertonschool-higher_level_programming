3ab58683cac91b64a485cd8645db9d0b1c020438
Amy <amy.tai0120@gmail.com>
2018-08-01 02:18:41 +0000

    Q6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Module to find peak of unsorted list of int
+'''
+
+
+def find_peak(list_of_integers):
+    last = len(list_of_integers) - 1
+    mid = int(last / 2)
+
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[mid]
+
+    try:
+        if list_of_integers[mid - 1] <= list_of_integers[mid] and \
+                list_of_integers[mid + 1] <= list_of_integers[mid]:
+            return list_of_integers[mid]
+
+    except IndexError:
+        if list_of_integers[mid + 1] > list_of_integers[mid]:
+            return list_of_integers[mid + 1]
+        else:
+            return list_of_integers[mid]
+
+    if list_of_integers[mid + 1] > list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[mid:])
+    else:
+        return find_peak(list_of_integers[:mid])
