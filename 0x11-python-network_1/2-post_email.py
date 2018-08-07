2c008d4d125b92caf7123e3046874b8609e9e9f8
Amy <amy.tai0120@gmail.com>
2018-08-07 06:22:20 +0000

    remove print stuff

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     arg = sys.argv[1]
     value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value).encode('utf-8')
-    req = urllib.request.Request(arg, data)
-    with urllib.request.urlopen(req) as resp:
+    with urllib.request.urlopen(arg, data) as resp:
         html = resp.read()
-        print('Your email is: {}'.format(html.decode('utf-8')))
+        print(html.decode('utf-8'))
