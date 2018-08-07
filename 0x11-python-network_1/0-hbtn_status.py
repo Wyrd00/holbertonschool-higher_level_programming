f19c821a62888b24d62eb49457e5383fe7a4fb74
Amy <amy.tai0120@gmail.com>
2018-08-07 06:10:45 +0000

    what??;

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,10 @@
 '''
 import urllib.request
 
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- uft8 content: {}'.format(html.decode("utf-8")))
+        print('\t- uft8 content: {}'.format(html.decode('utf-8')))
