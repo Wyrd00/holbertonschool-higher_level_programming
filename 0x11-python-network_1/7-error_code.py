2250b7c10e892a9fd57443c6c5cf636b34f1efca
Amy <amy.tai0120@gmail.com>
2018-08-07 06:38:37 +0000

    move line from try

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,8 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
+    r = requests.get(argv[1])
     try:
-        r = requests.get(argv[1])
         r.raise_for_status()
         print(r.text)
     except requests.exceptions.HTTPError:
