f842f023b1e2982aff1f15bab321b8fd51992c54
Amy <amy.tai0120@gmail.com>
2018-08-07 05:20:49 +0000

    initial commit for 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+    script that takes in letter and send POST req
+    to URL with letter as parameter
+'''
+
+import requests
+from sys import argv
+
+try:
+    arg = argv[1]
+except IndexError:
+    arg = ""
+
+try:
+    payload = { 'q': arg }
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    new_dict = r.json()
+    print("[{}] {}".format(new_dict['id'], new_dict['name']))
+except KeyError:
+    print("No result") 
+except:
+    print("Not a valid JSON")
