74c23770e6878f1d9f3cdcec7796422f99f8b430
Amy <amy.tai0120@gmail.com>
2018-08-07 06:13:09 +0000

    3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+    Display body or response decoded in utf-8
+    with Error Code
+'''
+
+import urllib.request as request
+from sys import argv
+import urllib.error as Error
+
+if __name__ == "__main__":
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as resp:
+            print(resp.read().decode('utf-8'))
+    except Error.HTTPError as e:
+        print("Error code: {}".format(e.code))    
