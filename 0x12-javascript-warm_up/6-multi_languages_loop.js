0da585ca64968e4d14c88926946b6e646640fd3d
Amy <amy.tai0120@gmail.com>
2018-08-28 23:02:37 +0000

    semistandard Q6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,8 @@
 
 const statements = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < statements.length; i++) {
-    console.log(statements[i])
-};
+  console.log(statements[i]);
+}
 
 // using a forEach
 /* statements.forEach((element) => {
