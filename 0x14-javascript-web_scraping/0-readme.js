8ef7be0135bf54ea5e82340cf9ff01ecfd60bbca
Amy <amy.tai0120@gmail.com>
2018-09-05 21:28:15 +0000

    fix file path

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 let filename = process.argv[2];
-fs.readFile('./' + filename, 'utf8', (err, data) => {
+fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
